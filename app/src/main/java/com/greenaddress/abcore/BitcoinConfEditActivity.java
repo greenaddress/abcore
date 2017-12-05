fc22a156b9477ef181191cac250d58fc1053e2f4
Lawrence Nahum <lawrence@greenaddress.it>
2017-12-05 20:51:51 +0100

    add permission for EXTERNALSTORAGE (for Android M onwards)

--- app/src/main/java/com/greenaddress/abcore/BitcoinConfEditActivity.java
+++ app/src/main/java/com/greenaddress/abcore/BitcoinConfEditActivity.java
@@ -1,6 +1,10 @@
 package com.greenaddress.abcore;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.EditText;
@@ -26,6 +30,15 @@ public class BitcoinConfEditActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED &&
+                !ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE))
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        0);
+
         // save file
         OutputStream f = null;
         try {
