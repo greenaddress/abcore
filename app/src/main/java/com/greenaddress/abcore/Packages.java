fadfa97fab024f1d88d5de18902a062c2340c239
Lawrence Nahum <lawrence@greenaddress.it>
2018-08-23 13:23:07 +0200

    update knots to v0.16.2.knots20180730

--- app/src/main/java/com/greenaddress/abcore/Packages.java
+++ app/src/main/java/com/greenaddress/abcore/Packages.java
@@ -8,6 +8,8 @@ class Packages {
     final static String BITCOIN_NDK = "0.16.2";
 
     private final static String URL = "https://github.com/greenaddress/bitcoin_ndk/releases/download/v0.16.2/%s_bitcoin%s.tar.gz";
+    private final static String URL_KNOTS = "https://github.com/greenaddress/bitcoin_ndk/releases/download/v0.16.2.knots20180730/%s_bitcoin%s.tar.gz";
+
 
     final static List<String> NATIVE_CORE = Arrays.asList(
             "3820562arm-linux-androideabiad9ddaa9d9f18e8bb2ce2e4e08a43778d54669535faecc3d791f6a2065a2b4ee",
@@ -17,18 +19,17 @@ class Packages {
     );
 
 
-    final static List<String> NATIVE_KNOTS =
-            Arrays.asList(
-                    "4007064arm-linux-androideabi6a7079c155c73ce13d5be613b4f0c2a347d6e4522901a429d6f6462d84aba84c",
-                    "4003310aarch64-linux-android71eda00b437eca28097b557e6d1df0527e492b547b9d2e5b8942396e1f7ac7a7",
-                    "4416797x86_64-linux-androidc0bbf5930f43619d96b5041ec8adc40d71c61b2d21deec06891819a8d099819f",
-                    "4322555i686-linux-androidf62702523815072f051c202be7b8d988e1c4f57d85059631d3d09db4c15257d7"
-            );
+    final static List<String> NATIVE_KNOTS = Arrays.asList(
+            "4003537arm-linux-androideabiaa026c69d4f2b51efe5730dea8cbb9575ff7e6ca651a29cd7e2b970ac48a497b",
+            "3998145aarch64-linux-android32faebb115926f731bfb0b5f2ee524d8876c90c2ed677e9e4844d73345770bc0",
+            "4412806x86_64-linux-android6fc81027363625f88a3d5c1bf3338838c33c47f5e6ffe43a7f6aec3bc4b1f39a",
+            "4320618i686-linux-android1a036a5b06ff9b88f7b7b60c973a8c0913d8194b3f8ed2fb0b6260430f657f87"
+    );
 
     static String getPackageUrl(final String distro, final String arch) {
         if (distro.equals("core")) {
             return String.format(URL, arch, "");
         }
-        return String.format(URL, arch, "knots");
+        return String.format(URL_KNOTS, arch, "knots");
     }
 }
