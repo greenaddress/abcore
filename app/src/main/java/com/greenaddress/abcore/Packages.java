ebd58ebfede24d6360b4257da82d6371571dab30
Lawrence Nahum <lawrence@greenaddress.it>
2019-01-02 17:34:06 +0100

    update abcore to bitcoin core and knots 0.17.1.x with latest ndk/debian stretch

--- app/src/main/java/com/greenaddress/abcore/Packages.java
+++ app/src/main/java/com/greenaddress/abcore/Packages.java
@@ -5,27 +5,27 @@ import java.util.List;
 
 class Packages {
 
-    final static String BITCOIN_NDK = "0.17.0";
-    final static String BITCOIN_KNOTS_NDK = "0.16.3";
+    final static String BITCOIN_NDK = "0.17.1";
+    final static String BITCOIN_KNOTS_NDK = "0.17.1";
 
 
-    private final static String URL = "https://github.com/greenaddress/bitcoin_ndk/releases/download/v0.17.0/%s_bitcoin%s.tar.gz";
-    private final static String URL_KNOTS = "https://github.com/greenaddress/bitcoin_ndk/releases/download/v0.16.3/%s_bitcoin%s.tar.gz";
+    private final static String URL = "https://github.com/greenaddress/bitcoin_ndk/releases/download/v0.17.1/%s_bitcoin%s.tar.gz";
+    private final static String URL_KNOTS = "https://github.com/greenaddress/bitcoin_ndk/releases/download/v0.17.1/%s_bitcoin%s.tar.gz";
 
 
     final static List<String> NATIVE_CORE = Arrays.asList(
-            "3825885arm-linux-androideabib4b3be79c859c83ca02ac12b70fceb3e800eb15580addd7b48bb1b896a32c302",
-            "3903585aarch64-linux-android51c129708b2ae4a4bc223fff1b486d6a62cabbfaaf6b8b7a9ef3aa71ac8f1ab8",
-            "4250511x86_64-linux-androide688b8592e660c52c39aba98ec141d1799f501bdd23af2f9e3fdb60541b3b466",
-            "4127210i686-linux-android5ae74105f98e0e0737ef40950f0078d3b504c50b34348bfb93a76f7cc37379da"
+            "3836183arm-linux-androideabia73a2e62e33ae994ecda021b130c55672a4dd3f53977b8427a4e45d5097f56a4",
+            "3910122aarch64-linux-androidaa45b42f08a4d7b71f12758f92155b319abfc8c04f55fba47bff1abbbfc0bdfa",
+            "4260400x86_64-linux-androidfeb2b7614111e7591cb4cfaa82fe31f97953324ccac55e25e2a8a26a24b2be60",
+            "4138985i686-linux-android2c6fda14756aa9fd08068bb85f5589f31e2c490f88e12c90b276256cb672c6df"
     );
 
 
     final static List<String> NATIVE_KNOTS = Arrays.asList(
-            "4004146arm-linux-androideabi241d614774733ab68d98299e6eb9c313238ad27830e8364d03006f7543b00a34",
-            "3998141aarch64-linux-androidb415944c32948f4ceab4447087e5031aed06acc57120f7a45f55300862258373",
-            "4411787x86_64-linux-androidcf05c6d6be9d628a0bc6fc2c3bf9202e84f9afc1d6a0db3f995aa59261fffa5d",
-            "4320595i686-linux-android14c0d2249d91b679f69bea52ffe1343284142c4dde02622aefee0c30bee86c9c"
+            "3967502arm-linux-androideabi9ae8fbbdfe43178474e9f876e4d22f199cec98a94995ad84ef37e3172b499b25",
+            "4037180aarch64-linux-android96a76ad89f57b3a36e74f4de179ca9f0e9e1355558b423ea7229d666b5cb6c22",
+            "4402426x86_64-linux-android98a48e390c7b527135747e75b4925232e812204c99b425ffd9140541631f87f2",
+            "4278455i686-linux-android17354bd00d840f3cfd90df9031ba1249ac408a994a61c555fc7ddcc7329fdd1c"
     );
 
     static String getPackageUrl(final String distro, final String arch) {
