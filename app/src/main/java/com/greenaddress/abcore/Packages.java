b382a63adf2603797b9c9e6f2b17fcc5d696eb9e
Lawrence Nahum <lawrence@greenaddress.it>
2019-10-16 03:42:33 +0200

    update to latest bitcoin_ndk (tor fix)

--- app/src/main/java/com/greenaddress/abcore/Packages.java
+++ app/src/main/java/com/greenaddress/abcore/Packages.java
@@ -13,26 +13,26 @@ class Packages {
     // size in bytes, arch, and sha256 of the native build
 
     final static List<String> NATIVE_CORE = Arrays.asList(
-            "4848136aarch64-linux-android23b3e92aa838dfa5f05016cdb88d6e20e67cf96c93284100f1c97459901cc10b",
-            "4897328arm-linux-androideabi0d700006529af4fafb4d022baf7b3287b1e68abb4c50a4818e70f020a473e6cc",
-            "5648180i686-linux-androida71932d05fde2ae475b11db14af2ec84026838d442a0a9b8ecfc1cf3517d1ee1",
-            "5657736x86_64-linux-androidbf3d6a6ec84dfb77d7076c4a593d743a2958b59c676a01d2c4bfe1363057b50b"
+            "4407008aarch64-linux-android38321423557aa95804e3093a72eae38ca4d9b53ee510befb7aabd13f71f4754b",
+            "4300184arm-linux-androideabie3bb5f01749eb8e670796bf36b1c230104ab88995b851e1fa51f2cec25d38aea",
+            "5066156i686-linux-android97a0cbd3c9009571a6d426b860fb88001d095547751739e23a3976ed4ab81dc6",
+            "5158500x86_64-linux-android2f270e610766576a38ffd777c807f95b50da3d7b12f4face80f361e3932bc4d6"
     );
     final static List<String> NATIVE_KNOTS = Arrays.asList(
-            "4841948aarch64-linux-androidbbe3a2d02d10794d914edf387023b76e9cd3a4cce196c00bd80d88172c427a6d",
-            "4891640arm-linux-androideabid583a59b63ec58fc6a7f11f42cb87c849ce18b16ad23631c369b4167ef355665",
-            "5639564i686-linux-android61f00737995d4519bfe9715231e7a37c50b5268de50433d3db613e48577b98e8",
-            "5650972x86_64-linux-androida6b3323ea866baea07e62cbe7d0997f940b86f38579f1458dca5f9ad539ba640"
-    );
+            "4400784aarch64-linux-android2cd4cc247a4515ead82415da341cf193cf7d9fcf62fe6703cff55dd48975334f",
+            "4292692arm-linux-androideabi61ac2979a519fec0d4a692c1ed03e78d4024bc59dc60613b11f4b6189fca4219",
+            "5058384i686-linux-android24f1246e1d1c58c618990d002256cdbb67dbcb2d0b3b3159db20319e4df4b8c2",
+            "5150340x86_64-linux-android4a80324170438489d3c0d590970092b929f5cc6165ccc6cc0096b206f11f490b"
+            );
     final static List<String> NATIVE_LIQUID = Arrays.asList(
-            "4985268aarch64-linux-android57005da16d4b6cadf47f1e320f65dfec6653f94bcbab301f3b8e681f187c6f44",
-            "5052320arm-linux-androideabi478f37cf1a2ef2720457b8b6a4814c5c5738e250a480bc9680efb66c2c1419c5",
-            "5823492i686-linux-android4400bc2779d3084b2ad432c6bc22e420dc4872548d3e621db67b543f734ba38d",
-            "5834616x86_64-linux-androidc72ac213209a9a2b78a6d425366ecab3223c6a2e22f55808f6d71f4ae7a91104"
+            "4546516aarch64-linux-androidf6ea59f7989a090dc381ccda020ea555d2bc019e7756a97e60f9381c2b9da4a6",
+            "4450572arm-linux-androideabi3bb3418fdf0f94f0cea7f631bec1d003d7eb8a1b51e4d86827729045a01fd1a4",
+            "5238848i686-linux-androide9720789ab4cee32d12e31ac9968c89067320fb205c9cab8d494436d9777abe1",
+            "5335644x86_64-linux-androidcd8a855b7cabc341dd5b4f26b51461af3d93d4ff989d91c80eafc0c55e3b8407"
     );
-    private final static String URL = "https://github.com/greenaddress/bitcoin_ndk/releases/download/v0.18.1b/%s_bitcoin%s.tar.xz";
-    private final static String URL_KNOTS = "https://github.com/greenaddress/bitcoin_ndk/releases/download/v0.18.1b/%s_bitcoin%s.tar.xz";
-    private final static String URL_LIQUID = "https://github.com/greenaddress/bitcoin_ndk/releases/download/v0.18.1b/%s_%s.tar.xz";
+    private final static String URL = "https://github.com/greenaddress/bitcoin_ndk/releases/download/v0.18.1c/%s_bitcoin%s.tar.xz";
+    private final static String URL_KNOTS = "https://github.com/greenaddress/bitcoin_ndk/releases/download/v0.18.1c/%s_bitcoin%s.tar.xz";
+    private final static String URL_LIQUID = "https://github.com/greenaddress/bitcoin_ndk/releases/download/v0.18.1c/%s_%s.tar.xz";
 
     static String getPackageUrl(final String distro, final String arch) {
         if ("core".equals(distro)) {
